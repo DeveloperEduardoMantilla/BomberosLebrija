3baa38310c1a86a49d107fa7556e15ded5823c71
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-10-23 16:41:17 +0200

    feat(BomberosLebrija): :tada: Estructura base del proyecto bomberos lebrija
    Se carga la estructura base del proyecto bomberos de lebrija, esta estructura fue creada a travers de vite como gestor de paquetes
    
    EduardoMantillaCampus

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h2>Bomberos</h2>
+  </React.StrictMode>,
+)
