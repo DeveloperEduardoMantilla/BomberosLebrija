a237cb651f9bcd9613aaa547e9cfb456a27c37e0
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-11-29 19:13:22 -0500

    feat(BomberosLebrija): :sparkles: BackEnd - Levantamiento Servidor Express
    Se realiza el levantamiento del servidor con node-express
    
    EduardoMantilla

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express  from 'express';
+import dotenv from 'dotenv';
+import cors from "cors";
+
+let app = express();
+app.use(express.json());
+dotenv.config();
+app.use(cors());
+
+let server = JSON.parse(process.env.MY_SERVER);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Reemplaza con la URL de tu frontend
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+app.listen(server,()=>{
+    console.log(`http://${server.hostname}:${server.port}`);
+})
