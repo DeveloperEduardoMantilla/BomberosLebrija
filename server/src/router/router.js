5d9b3227acac09f91fb4eb321609cdbb6438b9db
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-11-29 19:19:22 -0500

    feat(BomberosLebrija): :sparkles: BackEnd - Implementacion de Express-Rate-limit
    Se realiza la implementacion de Express-Rate-Limit para controlar el consumo de las apis
    
    EduardoMantilla

--- server/src/router/router.js
+++ server/src/router/router.js
@@ -1,6 +1,8 @@
 import express  from 'express';
+import {limitApi} from '../limits/limit.js';
 
 let appRouter = express();
+appRouter.use(limitApi());
 
 appRouter.get('/',(req, res) => {
     res.send({message:`Bienvenidos a la api de Bomberos de Lebrija`});
